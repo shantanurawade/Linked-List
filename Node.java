7aebf9e3053486b96bf79fa518c890b9a74bcbdf
shantanurawade <103266755+shantanurawade@users.noreply.github.com>
2024-07-20 11:39:21 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,7 +1,11 @@
 class Node{
+    // This is variable to store data in the list 
     int data;
+    // This is the index to maintain index
     int index;
+    // This is pointer to next node for list  
     Node next;
+    // Constructor to initialize the data field in list
     Node (int data){
         this.data = data;
     }
